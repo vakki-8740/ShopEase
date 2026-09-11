ecc2a32a9d3308eca5cb8d1bb3732ad37b00379a
vakki-8740 <lightsir8740@gmail.com>
2026-09-11 19:44:11 +0530

    Simplify cart.js - fix buyFromBtn not defined error

--- user-panel/cart.js
+++ user-panel/cart.js
@@ -1,35 +1,24 @@
 // Buy Now - goes directly to checkout
 
-let currentUser = null;
-
-// Get current user from auth.js
-function setCurrentUser(user) {
-    currentUser = user;
-}
-
 // Called from button click - reads data attributes
 function buyFromBtn(btn) {
-    const id = btn.getAttribute('data-id');
-    const name = btn.getAttribute('data-name');
-    const price = parseInt(btn.getAttribute('data-price'));
-    const icon = btn.getAttribute('data-icon');
+    var id = btn.getAttribute('data-id');
+    var name = btn.getAttribute('data-name');
+    var price = parseInt(btn.getAttribute('data-price'));
+    var icon = btn.getAttribute('data-icon');
 
-    buyNow(id, { name: name, price: price, icon: icon });
-}
-
-function buyNow(productId, product) {
-    const orderData = {
+    var orderData = {
         items: [{
-            id: productId,
-            name: product.name,
-            price: product.price,
-            icon: product.icon,
+            id: id,
+            name: name,
+            price: price,
+            icon: icon,
             qty: 1
         }],
-        total: product.price,
-        userId: currentUser ? currentUser.uid : 'guest',
-        userName: currentUser ? (currentUser.displayName || currentUser.email) : 'Guest User',
-        userEmail: currentUser ? currentUser.email : ''
+        total: price,
+        userId: 'guest',
+        userName: 'Guest User',
+        userEmail: ''
     };
 
     localStorage.setItem('pendingOrder', JSON.stringify(orderData));
@@ -39,9 +28,3 @@ function buyNow(productId, product) {
 function toggleMobileMenu() {
     document.querySelector('.nav-links').classList.toggle('show');
 }
-
-// Make functions globally accessible
-window.buyNow = buyNow;
-window.buyFromBtn = buyFromBtn;
-window.toggleMobileMenu = toggleMobileMenu;
-window.setCurrentUser = setCurrentUser;
